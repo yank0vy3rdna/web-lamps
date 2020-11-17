739c14b322a5bf3f0d675d145912c3350d95ae86
yank0vy3rdna <yankovyerdna@yandex.ru>
2020-11-17 20:39:14 +0300

    feat: handlers init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
-from handlers import getInfo
+from handlers import getInfo, lampOff, lampOn
 
 
 def setup(app: Flask):
     getInfo.setup(app)
+    lampOff.setup(app)
+    lampOn.setup(app)
