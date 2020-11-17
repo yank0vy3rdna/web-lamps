6cb1433dbec3b514ff2e73fe16458d92e535207f
yank0vy3rdna <yankovyerdna@yandex.ru>
2020-11-17 21:53:04 +0300

    fix: login realized

--- handlers/auth/login.py
+++ handlers/auth/login.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 
 def login():
-    return "token"
+    login = request.args.get('login')
+    password = request.args.get('password')
+    if login == "admin" and password == "qwerty123":
+        return "token"
+    raise ValueError("Auth error")
 
 
 def setup(app: Flask):
-    app.add_url_rule("/auth/login/", 'login', login)
+    app.add_url_rule("/auth/login/", 'login', login, methods=['POST'])
